81de9dfc6c159b1676be59e486f7ef62c42b8037
Benjamin Kyhn <benkyhn@hotmail.com>
2020-02-15 17:54:31 +0100

    Added exercise 14.7

---
+++ src/Exercise_14_07.java
@@ -0,0 +1,33 @@
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Exercise_14_07 extends Application {
+    @Override
+    public void start(Stage primaryStage){
+        GridPane pane = new GridPane();
+        pane.setPadding(new Insets(5, 5, 5, 5));
+        pane.setHgap(5);
+        pane.setVgap(5);
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                TextField text = new TextField();
+                text.setPrefColumnCount(1);
+                text.setText(String.valueOf((int)(Math.random() * 2)));
+                pane.add(text, i, j);
+                pane.setHalignment(text, HPos.CENTER);
+                pane.setValignment(text, VPos.CENTER);
+            }
+        }
+
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
